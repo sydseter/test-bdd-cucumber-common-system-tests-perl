727885971b376101aaa2cf75341151cb34ee9669
Johan Sydseter <johan.sydseter@startsiden.no>
2013-01-06 23:48:44 +0100

    Added new tests

--- installed_packages_steps.pl
+++ installed_packages_steps.pl
@@ -1,27 +1,45 @@
 #!/usr/bin/perl
-# @author Johan Sydseter, <johan.sydseter@startsiden.no>
-# @author Thomas Malt, <thomas.malt@startsiden.no>
-
-# Feature steps for verifying that a debian package is installed
-
 use strict;
 use warnings;
 
 use Test::More;
 use Test::BDD::Cucumber::StepFile;
 use Method::Signatures;
-use Data::Dump;
 
-##
-# Scenario: Check that the right debian packages are installed
-##
+=head1 AUTHORS
 
-# Verify that all necessary packages is installed
-When qr/this debian "(.+)"/, func ($c) {
+Johan Sydseter, C<< <johan.sydseter@startsiden.no> >>
+Thomas Malt, C<< <thomas.malt@startsiden.no> >>
+
+=cut
+
+=head1 DESCRIPTION
+
+These steps tests debian packages
+
+=cut
+
+=head3 Scenario: Check that the right debian packages are installed
+
+=cut
+
+=head4 When "<package>" is installed on the system
+
+=cut
+When qr/"(.+)" is installed on the system/, func ($c) {
     my $package = $1;
     ok(has_installed_package($package), "Verify that the '". $package . "' is installed");
+    $c->stash->{scenario}->{package} = $package;
 };
 
+=head4 Then it has this version number "<version>"
+
+=cut
+Then qr/it has this version number "(.+)"/, func ($c) {
+    my $version = $1;
+    my $package = $c->stash->{scenario}->{package};
+    ok(package_has_version($package,$version));
+};
 
 # Verify that all necessary modules is made available to the perl environment
 Then qr/this perl "(.+)"/, func ($c) {
@@ -58,3 +76,18 @@ sub has_installed_package {
     return undef;
 }
 
+sub package_has_version {
+  my ($package,$version) = @_;
+
+  my $actual_version = qx(dpkg -l ${package} | tail -n 1);
+
+  ok(1,"The actual version: ${actual_version}");
+
+  chomp $actual_version;
+
+  if ( $actual_version =~ m/$version/) {
+      return 1;
+  }
+
+  return 0;
+}
